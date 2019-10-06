b8c39d181936e42a94aeedab97daafa519ea69ac
Lucas Moeskops <lucasmoeskops@gmail.com>
2019-10-06 22:19:09 +0200

    Missing Integer assignment

---
+++ missing_integer/main.py
@@ -0,0 +1,40 @@
+"""
+This is a demo task.
+
+Write a function:
+
+def solution(A)
+
+that, given an array A of N integers, returns the smallest positive integer (greater
+than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying,
+publication or disclosure prohibited.
+"""
+
+__author__ = 'Lucas Moeskops'
+
+
+def solution(A):
+    C = [False] * (len(A) + 1)
+
+    for n in A:
+        if 0 < n <= len(A):
+            C[n] = True
+
+    for i in range(1, len(A) + 1):
+        if not C[i]:
+            return i
+
+    return len(A) + 1
